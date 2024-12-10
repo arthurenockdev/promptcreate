62b39d73a1f554f49c4242c24802879a0064c9d8
arthurenockdev <arthurenock741@gmail.com>
2024-12-10 20:35:06 +0000

    Reverted improvements to maintain original structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,43 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  headers: async () => [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'X-DNS-Prefetch-Control',
+          value: 'on'
+        },
+        {
+          key: 'Strict-Transport-Security',
+          value: 'max-age=63072000; includeSubDomains; preload'
+        },
+        {
+          key: 'X-XSS-Protection',
+          value: '1; mode=block'
+        },
+        {
+          key: 'X-Frame-Options',
+          value: 'SAMEORIGIN'
+        },
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff'
+        },
+        {
+          key: 'Referrer-Policy',
+          value: 'origin-when-cross-origin'
+        },
+        {
+          key: 'Content-Security-Policy',
+          value: "default-src 'self' https://cal.com; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cal.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://cal.com;"
+        }
+      ]
+    }
+  ],
+  images: {
+    domains: ['cal.com'],
+  }
+};
 
 export default nextConfig;
